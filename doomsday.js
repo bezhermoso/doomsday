b779004ea6f6d04542a733f085d8e6c837166fc6
Bez Hermoso <bezalelhermoso@gmail.com>
2016-01-05 13:57:32 -0800

    Initial commit

---
+++ doomsday.js
@@ -0,0 +1,16 @@
+var Doomsday = require("./lib/doomsday.js");
+var Days = require("./lib/days");
+var parseArgs = require("minimist");
+
+var args = parseArgs(process.argv.slice(2));
+
+var date = args._.pop();
+
+if (!date) {
+  console.error("Please provide date.");
+  process.exit(1);
+}
+
+var result = Doomsday(date);
+
+console.log(result);
